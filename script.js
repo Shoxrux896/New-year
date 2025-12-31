c740ec51cc25533e8a66f08fb87674efa4114447
Shoxrux896 <sotrukak@gmail.com>
2025-12-31 16:36:37 +0500

    Update

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const CONFIG = {
     stars: 100,
     snowflakes: 30,
     fireworksPerClick: 5,
-    targetDate: Date.now() + 20000, // Demo mode: 20s countdown
+    targetDate: new Date('2026-01-01T00:00:00').getTime(),
 };
 
 /**
